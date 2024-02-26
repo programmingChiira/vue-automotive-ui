25301d65f4ca8ffd0f54cf7e5eb9ffcf7f870d8f
DennisChiira <programmingchiira@gmail.com>
2024-02-26 15:10:22 +0300

    looking to use a sidebar navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-auto bg-dark sticky-top">
+        <div class="d-flex flex-sm-column flex-row flex-nowrap bg-dark align-items-center sticky-top">
+          <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip"
+            data-bs-placement="right" data-bs-original-title="Icon-only">
+            <i class="bi-bootstrap fs-1"></i>
+          </a>
+          <ul
+            class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center justify-content-between w-100 px-3 align-items-center">
+            <li class="nav-item">
+              <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right"
+                data-bs-original-title="Home">
+                <i class="fa fa-home fs-1"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right"
+                data-bs-original-title="Dashboard">
+                <i class="fa fa-dashboard fs-1"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right"
+                data-bs-original-title="Orders">
+                <i class="fa fa-table fs-1"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right"
+                data-bs-original-title="Products">
+                <i class="fa fa-heart fs-1"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right"
+                data-bs-original-title="Customers">
+                <i class="fa fa-users fs-1"></i>
+              </a>
+            </li>
+          </ul>
+          <div class="dropdown">
+            <a href="#"
+              class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle"
+              id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="fa fa-user-circle h2"></i>
+            </a>
+            <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+              <li><a class="dropdown-item" href="#">New project...</a></li>
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm p-3 min-vh-100">
+        <router-view />
+        <h2>A glorious Bootstrap 5 icon sidebar menu!</h2>
+        <hr />
+        <h3>More content...</h3>
+        <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch.
+          Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote
+          bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical
+          Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan
+          cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable
+          cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+        <p>Ethical Kickstarter PBR asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter
+          artisan Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table
+          Williamsburg slow-carb readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel
+          whatever. Helvetica cray plaid, vegan brunch Banksy leggings +1 direct trade. Wayfarers codeply PBR selfies.
+          Banh mi McSweeney's Shoreditch selfies, forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny
+          pack art party Portland.</p>
+        <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing brunch.
+          Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape swag wolf squid tote
+        bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu YOLO Etsy, direct trade ethical
+        Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic Bushwick artisan
+        cliche semiotics ugh synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable
+        cardigan, Williamsburg master cleanse hella DIY 90's blog.</p>
+    </div>
+  </div>
+</div></template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import NavBar from './views/NavBar.vue';
 
-nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    // NavBar
+  }
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
